40fc8ed01f28b7cc0ca4911bbedaf97f1985746d
Ansh Singh <37961534+invinciblevenom@users.noreply.github.com>
2022-01-14 19:33:37 +0530

    Update heatmap.py
    Debug Code to show output in PyCharm

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-flight_data = sns.load_dataset('flight')
+flight_data = sns.load_dataset('flights') 
 flight_data.head()
 flight_data = flight_data.pivot('month', 'year', 'passengers')
-flight_data
 sns.heatmap(flight_data)
+plt.show()
